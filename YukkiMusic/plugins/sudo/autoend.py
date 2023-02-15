c302cc74d91e0346b137ff3bb8f41f1c25f5e963
ekber97 <121440920+ekber97@users.noreply.github.com>
2023-02-16 00:10:22 +0400

    Update autoend.py

--- YukkiMusic/plugins/sudo/autoend.py
+++ YukkiMusic/plugins/sudo/autoend.py
@@ -1,11 +1,3 @@
-#
-# Copyright (C) 2021-2022 by TeamYukki@Github, < https://github.com/TeamYukki >.
-#
-# This file is part of < https://github.com/TeamYukki/YukkiMusicBot > project,
-# and is released under the "GNU v3.0 License Agreement".
-# Please see < https://github.com/TeamYukki/YukkiMusicBot/blob/master/LICENSE >
-#
-# All rights reserved.
 
 from pyrogram import filters
 
@@ -22,7 +14,7 @@ AUTOEND_COMMAND = get_command("AUTOEND_COMMAND")
 
 @app.on_message(filters.command(AUTOEND_COMMAND) & SUDOERS)
 async def auto_end_stream(client, message):
-    usage = "**Usage:**\n\n/autoend [enable|disable]"
+    usage = "**İstifadəsi:**\n\n/autoend [enable|disable]"
     if len(message.command) != 2:
         return await message.reply_text(usage)
     state = message.text.split(None, 1)[1].strip()
@@ -30,10 +22,10 @@ async def auto_end_stream(client, message):
     if state == "enable":
         await autoend_on()
         await message.reply_text(
-            "Auto End Stream Enabled.\n\nBot will leave voice chat automatically after 3 mins if no one is listening with a warning message.."
+            "Yayımı avtomatik bitirmə aktivləşdirildi.\n\nHeç kim xəbərdarlıq mesajı ilə qulaq asmasa, bot 3 dəqiqədən sonra səsli söhbəti avtomatik tərk edəcək..."
         )
     elif state == "disable":
         await autoend_off()
-        await message.reply_text("Auto End Stream Disabled.")
+        await message.reply_text("Yayımı avtomatik bitirmə deaktiv edilib.")
     else:
         await message.reply_text(usage)
