94b3692d3ddf02234a76f83e2fef6d0ddeec76b3
ekber97 <121440920+ekber97@users.noreply.github.com>
2023-02-22 00:28:43 +0400

    Update inlinequery.py

--- YukkiMusic/utils/inlinequery.py
+++ YukkiMusic/utils/inlinequery.py
@@ -1,11 +1,4 @@
-#
-# Copyright (C) 2021-2022 by TeamYukki@Github, < https://github.com/TeamYukki >.
-#
-# This file is part of < https://github.com/TeamYukki/YukkiMusicBot > project,
-# and is released under the "GNU v3.0 License Agreement".
-# Please see < https://github.com/TeamYukki/YukkiMusicBot/blob/master/LICENSE >
-#
-# All rights reserved.
+
 
 from pyrogram.types import (InlineQueryResultArticle,
                             InputTextMessageContent)
@@ -15,56 +8,56 @@ answer = []
 answer.extend(
     [
         InlineQueryResultArticle(
-            title="Pause Stream",
-            description=f"Pause the current playout on group call.",
+            title="Yayımı dayandırın.",
+            description=f"Qrup daxilində cari oyunu dayandırın.",
             thumb_url="https://telegra.ph/file/c0a1c789def7b93f13745.png",
             input_message_content=InputTextMessageContent("/pause"),
         ),
         InlineQueryResultArticle(
-            title="Resume Stream",
-            description=f"Resume the ongoing playout on group call.",
+            title="Yayımı davam etdirin.",
+            description=f"Qrup daxilində davam edən musiqini davam etdirin.",
             thumb_url="https://telegra.ph/file/02d1b7f967ca11404455a.png",
             input_message_content=InputTextMessageContent("/resume"),
         ),
         InlineQueryResultArticle(
-            title="Mute Stream",
-            description=f"Mute the ongoing playout on group call.",
+            title="Yayım səsini söndürün.",
+            description=f"Qrup daxilində davam edən musiqini susdurun.",
             thumb_url="https://telegra.ph/file/66516f2976cb6d87e20f9.png",
             input_message_content=InputTextMessageContent("/mute"),
         ),
         InlineQueryResultArticle(
-            title="Unmute Stream",
-            description=f"Unmute the ongoing playout on group call.",
+            title="Yayım səsini aç.",
+            description=f"Qrup daxilində davam edən musiqinin səsini açın.",
             thumb_url="https://telegra.ph/file/3078794f9341ffd582e18.png",
             input_message_content=InputTextMessageContent("/unmute"),
         ),
         InlineQueryResultArticle(
-            title="Skip Stream",
-            description=f"Skip to next track. | For Specific track number: /skip [number] ",
+            title="Yayımı dəyiş.",
+            description=f"Növbəti trekə keçin. | Xüsusi trek nömrəsi üçün: /skip [sıra nömrəsi] ",
             thumb_url="https://telegra.ph/file/98b88e52bc625903c7a2f.png",
             input_message_content=InputTextMessageContent("/skip"),
         ),
         InlineQueryResultArticle(
-            title="End Stream",
-            description="Stop the ongoing playout on group call.",
+            title="Yayımı söndür.",
+            description="Qrup daxilində davam edən musiqini söndürün.",
             thumb_url="https://telegra.ph/file/d2eb03211baaba8838cc4.png",
             input_message_content=InputTextMessageContent("/stop"),
         ),
         InlineQueryResultArticle(
-            title="Shuffle Stream",
-            description="Shuffle the queued tracks list.",
+            title="Musiqini qarışdır.",
+            description="Növbəyə qoyulmuş musiqi siyahısını qarışdırın.",
             thumb_url="https://telegra.ph/file/7f6aac5c6e27d41a4a269.png",
             input_message_content=InputTextMessageContent("/shuffle"),
         ),
         InlineQueryResultArticle(
-            title="Seek Stream",
-            description="Seek the ongoing stream to a specific duration.",
+            title="Yayımı axtarın.",
+            description="Müəyyən bir müddətə davam edən axını axtarın.",
             thumb_url="https://telegra.ph/file/cd25ec6f046aa8003cfee.png",
             input_message_content=InputTextMessageContent("/seek 10"),
         ),
         InlineQueryResultArticle(
-            title="Loop Stream",
-            description="Loop the current playing music. | Usage: /loop [enable|disable]",
+            title="Yayımı təkrarla.",
+            description="Cari ifa olunan musiqini təkrərlayın. | İstifadəsi: /loop [enable|disable]",
             thumb_url="https://telegra.ph/file/081c20ce2074ea3e9b952.png",
             input_message_content=InputTextMessageContent("/loop 3"),
         ),
